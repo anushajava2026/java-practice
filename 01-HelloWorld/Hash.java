d5913e9b2d2ae98ed0417dd02fa6f604e6a613a4
kandra Anusha <kandraanusha10@gmail.com>
2026-07-03 20:59:15 +0530

    Create Hash.java

---
+++ 01-HelloWorld/Hash.java
@@ -0,0 +1,12 @@
+package hash;
+
+public class Hash {
+
+	public static void main(String[] args) {
+		System.out.println("########## ");
+		System.out.println("Hello ");
+		System.out.println("########## ");
+
+	}
+
+}
