0b1d37418693c6d2701ea930284011249ab900c7
kandra Anusha <kandraanusha10@gmail.com>
2026-07-03 20:56:45 +0530

    Create Pattern.java

---
+++ 01-HelloWorld/Pattern.java
@@ -0,0 +1,8 @@
+package pattern;
+public class Pattern {
+	public static void main(String[] args) {
+		System.out.println("========== ");	
+		System.out.println("Java ");	
+		System.out.println("========== ");	
+	}
+}
