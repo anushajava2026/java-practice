0278c859d77c07c45627fa0325a8e0b58ed9543a
kandra Anusha <kandraanusha10@gmail.com>
2026-07-03 20:33:59 +0530

    Create Fruits.java

---
+++ 01-HelloWorld/Fruits.java
@@ -0,0 +1,9 @@
+package fruits;
+public class Fruits {
+	public static void main(String[] args) {
+		System.out.println("Banana");
+		System.out.println("Apple");
+		System.out.println("Orange");
+		System.out.println("Mango");
+	}
+}
